82763cc31a9b86751e766f2b00dd37080c006436
LuckyAmaral <lucas.amaral.olivera@gmail.com>
2024-07-09 15:06:06 -0300

    Ragdoll

---
+++ 67 bits/Assets/Scripts/NPC.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPC : MonoBehaviour
+{
+    ///<sumary>
+    ///Realiza as funções basicas do NPC, como andar e Ragdoll
+    ///</sumary>
+    [SerializeField] private Animator m_Anim;
+    [SerializeField] private List<Collider> m_Collider;
+    [SerializeField] private List<HingeJoint> m_Joints;
+    [SerializeField] private List<Rigidbody> m_Rb;
+
+    ///<sumary>
+    ///define que não esta como no modo ragdoll quando iniciado
+    ///</sumary>
+    void Start()
+    {
+        Ragdol(false);
+    }
+
+    ///<sumary>
+    ///Modifica o NPC para botar e tirar no estado de ragdoll
+    ///</sumary>
+    ///<param name = "RagOn">
+    ///booleana para decidir se esta ligado ou desligado
+    ///</param>
+    public void Ragdol(bool RagOn){ 
+            m_Anim.enabled = !RagOn;
+        foreach (var col in m_Collider) {
+            col.enabled = RagOn;
+        }
+        foreach (var rb in m_Rb) {
+            rb.isKinematic  = !RagOn;
+        }
+    }
+
+    void Update(){
+        if (Input.GetKeyDown(KeyCode.Space)){
+            Ragdol(true);
+        }
+    }
+}
